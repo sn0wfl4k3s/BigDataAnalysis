31a7c24f27aefeb7df86bb78419df82111181a2d
Flakes <ed.afram@gmail.com>
2020-06-08 23:57:10 -0300

    apache spark works.

--- mySparkApp/mySparkApp/Program.cs
+++ mySparkApp/mySparkApp/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Spark.Sql;
+using System;
 
 namespace mySparkApp
 {
@@ -6,7 +7,29 @@ namespace mySparkApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a Spark session.
+            SparkSession spark = SparkSession
+                .Builder()
+                .AppName("word_count_sample")
+                .GetOrCreate();
+
+            // Create initial DataFrame.
+            DataFrame dataFrame = spark.Read().Text("input.txt");
+
+            // Count words.
+            DataFrame words = dataFrame
+                .Select(Functions.Split(Functions.Col("value"), " ").Alias("words"))
+                .Select(Functions.Explode(Functions.Col("words"))
+                .Alias("word"))
+                .GroupBy("word")
+                .Count()
+                .OrderBy(Functions.Col("count").Desc());
+
+            // Show results.
+            words.Show();
+
+           // Stop Spark session.
+           spark.Stop();
         }
     }
 }
